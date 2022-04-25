a2f2496b65387024355fbd4ba22265db4801e690
mwezn <m.w.g.nelson@gmail.com>
2022-04-25 15:36:56 +0100

    Added more pics

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ function App() {
       <Route path="/0" element={<Description site={0} />}></Route>
       <Route path="/1" element={<Description site={1} />}></Route>
       <Route path="/2" element={<Description site={2} />}></Route>
+      <Route path="/3" element={<Description site={3} />}></Route>
+      <Route path="/4" element={<Description site={4} />}></Route>
+      <Route path="/5" element={<Description site={5} />}></Route>
+      <Route path="/6" element={<Description site={6} />}></Route>
       </Routes>
     </BrowserRouter>
   );
