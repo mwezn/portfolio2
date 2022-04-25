236f2bcbd8f716060c5e5778fb2c47f363f892f6
mwezn <m.w.g.nelson@gmail.com>
2022-04-25 13:51:31 +0100

    Adding hamburger animation to navbar

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import './description.css'
 
 const DescribePage =(props)=>{
     const [data,updateData]= useState([])
@@ -15,22 +17,23 @@ const DescribePage =(props)=>{
            console.log(myJson);
            updateData(myJson)
     
+
           });
     }
     useEffect(()=>{
         getData()
     
       },[])
-     
+
     return (
-        <div>
+        <div className="describePage">
             {
             data.length>0?
-            <div>
+            <div className='description'>
                 <h1>{data[props.site].name}</h1>
                 <p>{data[props.site].description}</p>
-                <img alt="img"src={data[props.site].pics}></img>
-                <a target='_blank' rel="noreferrer"href="http://ec2-18-191-242-179.us-east-2.compute.amazonaws.com/">The app is running here</a>
+                <Carousel>{data[props.site].pics.map(d=><img alt={d} src={d}></img>)}</Carousel>
+                <a target='_blank' rel="noreferrer" href={data[props.site].livelink}>The app is running here</a>
             </div>
             :<h1>Loading</h1>
             }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,34 @@
 import React from "react";
 import './navbar.css'
 const Navbar =()=>{
+    let menuopen=false;
+    let menubtn=document.querySelector('.menu-btn')
     const show =(e)=>{
-        document.getElementById('hamburger').classList.toggle('active')
-         document.getElementById('close').classList.toggle('active')
-}
+        //document.getElementById('showbar').classList.toggle('active')
+        if(!menuopen){
+            menubtn.classList.add('open')
+            document.getElementById('showbar').classList.add('active')
+            menuopen=true;
+        }
+        else {
+            menubtn.classList.remove('open')
+            document.getElementById('showbar').classList.remove('active')
+            menuopen=false;
+        }
+    }
     return(
         <nav id="navbar">
+        <div className="menu-btn" onClick={e=>show(e)}>
+            <div className="menu-btn-burger"></div>
+        </div>
          <div id="showbar">
-    <div id="close" class="close">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="45" viewBox="0 0 50 50" overflow="visible" stroke="#fff" stroke-width="5" >
-            <g color="#fff">
-            <line x2="50" y2="45" />
-            <line x1="50" y2="45" fill="currentcolor"/>
-            </g>
-         </svg>
-    </div>
-    <div id="hamburger" class="hamburger" onClick={e=>show(e)}>
-        <svg viewBox="0 0 100 80" width="40" height="40">
-            <g color="#fff">
-            <rect width="100" height="10" fill="currentcolor"></rect>
-            <rect y="30" width="100" height="10" fill="currentcolor"></rect>
-            <rect y="60" width="100" height="10" fill="currentcolor"></rect>
-            </g>
-        </svg>
-    </div>
-    <ul className="navbar-links">
-      <li><a href="#aboutme">About</a></li>
-      <li><a href="#projects">Projects</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-  </div>
-</nav>
+         <ul className="navbar-links">
+          <li><a href="#aboutme">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+         </ul>
+         </div>
+        </nav>
     )
 }
 
