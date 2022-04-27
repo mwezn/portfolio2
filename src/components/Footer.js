1a83484db46e9e4744808e6874a01b7c7af613aa
mwezn <m.w.g.nelson@gmail.com>
2022-04-27 13:59:36 +0100

    Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import './footer.css'
 const Footer =()=>{
     return(
         <div id="contact" className="footer">
+          <h3>Contact</h3>
           <ul>
-              <h3>Contact</h3>
             <li><a target="_blank" rel="noreferrer" href="https://github.com/mwezn">Github: mwezn</a></li>
             <li><a href="mailto:m.w.g.nelson@gmail.com">m.w.g.nelson@gmail.com</a></li>
             <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/michael-nelson-023472bb/">LinkedIn</a></li>
