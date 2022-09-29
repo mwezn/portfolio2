c3ee834e5c16540d4395e9f28ad13b795f7c08dd
mwezn <m.w.g.nelson@gmail.com>
2022-09-29 09:09:18 +0100

    errors with some sites

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,12 +5,6 @@ import img1 from '../assets/todo.png'
 import img2 from '../assets/medicalmaths.png'
 import img3 from '../assets/maja.png'
 import img4 from '../assets/client-posts.png'
-//import img5 from '../assets/repo.png'
-import img6 from '../assets/huel4.png'
-import fitness from '../assets/fitness1.png'
-//import jp from '../assets/j&p.png'
-
-
 
 
 const MainPage =()=>{
@@ -19,17 +13,19 @@ const MainPage =()=>{
     const onClick =(d,e)=>{
         window.location.href=d
     }
-    let myprojects=[
+    /*let myprojects=[
         {name: "Todo Emailer", tech:"React, Mongo, Express",img:img1, href:'0'},
         {name:"Medical graph", tech:"HTML/JS, D3.js",img:img2,href:'1'},
         {name: "Maja Notes", tech:"CSS/JS, Ejs, Jest", img:img3, href:'2'},
-        {name: "Fitness tracker", tech:"Docker, Mongo, HTML", img:fitness,href:'3'},
         {name: "Telegram notes", tech:"Docker, Mongo, HTML", img:img4, href:'4'},
-        {name: "Huel template", tech:'HTML/CSS', href:'5',img: img6},
-        //{name:"Jones & Palmer", tech:"React", img:jp}
-
+    ]*/
+    let mypages = [
+        {name: "Todo Emailer", tech:"React, Mongo, Express",img:img1, href:'https://todoemailer.herokuapp.com/'},
+        {name:"Medical graph", tech:"HTML/JS, D3.js",img:img2,href:"https://mwezn.github.io/DueDateVisualizer/"},
+        {name: "Maja Notes", tech:"CSS/JS, Ejs, Jest", img:img3, href:"https://majanotes22.herokuapp.com/"},
+        {name: "Telegram notes", tech:"Docker, Mongo, HTML", img:img4, href:"https://free-thoughts.netlify.app/"},
     ]
-    let result=myprojects.map((d,i)=>{
+    let result=mypages.map((d,i)=>{
         //let classId=((i+1)%8)===0?8:((i+1)%8)
     
         return (
