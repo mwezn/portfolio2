54ee90e365b800937cbe6d7f1ea3236e8b18c9c7
mwezn <m.w.g.nelson@gmail.com>
2022-10-29 13:19:59 +0100

    Trying netlify functions

---
+++ netlify/functions/binance.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+
+exports.handler = async () => {
+  let statusCode, data;
+
+  try {
+    const response = await fetch('https://api.binance.com/api/v1/klines?symbol=XRPUSDT&interval=1h');
+    data = await response.json();
+    statusCode = 200;
+  } catch (err) {
+    statusCode = err.statusCode || 500;
+    data = { error: err.message };
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify(data)
+  }
+};
