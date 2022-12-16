66a70daed8aa098cbf59671f1fca3c2587880ccc
mwezn <m.w.g.nelson@gmail.com>
2022-12-16 13:58:24 +0000

    using proxy for cryptod3

--- netlify/functions/binance.js
+++ netlify/functions/binance.js
@@ -4,7 +4,8 @@ exports.handler = async () => {
   let statusCode, data;
 
   try {
-    const response = await fetch('https://api.binance.com/api/v1/klines?symbol=ETHUSDT&interval=1h');
+    //const response = await fetch('https://api.binance.com/api/v1/klines?symbol=ETHUSDT&interval=1h');
+    const response=await fetch('http://13.40.184.90:8080/coins')
     data = await response.json();
     statusCode = 200;
   } catch (err) {
